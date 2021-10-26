090963b42b6b0cbc5256bbebb28f50ee2857199c
Defluxit <astroghost1@gmail.com>
2021-10-26 09:59:15 -0500

    Trying to figure out how to get item to be deleted

--- react-app/src/components/Items.js
+++ react-app/src/components/Items.js
@@ -1,11 +1,9 @@
-import React from 'react';
+import React from "react";
 
-import { Table, Button } from 'react-bootstrap';
+import { Table, Button } from "react-bootstrap";
 
 class Items extends React.Component {
-
   render() {
-
     return (
       <section>
         <h2>Items...</h2>
@@ -19,21 +17,22 @@ class Items extends React.Component {
             </tr>
           </thead>
           <tbody>
-            {
-              this.props.itemsList.map((item, idx) =>
-                <tr key={idx}>
-                  <td>{item.name}</td>
-                  <td>{item.description}</td>
-                  <td>
-                    <Button data-testid={`delete-button-${item.name}`}>Delete Item</Button>
-                  </td>
-                </tr>
-              )
-            }
+            {this.props.itemsList.map((item, idx) => (
+              <tr key={idx}>
+                <td>{item.name}</td>
+                <td>{item.description}</td>
+                <td>
+                  <Button
+                    onClick={props.delete}
+                    data-testid={`delete-button-${item.name}`}
+                  >
+                    Delete Item
+                  </Button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </Table>
-
-
       </section>
     );
   }
