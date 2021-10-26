1dd81afadbcac9de528ff62239b0e933e4b0dec1
Defluxit <astroghost1@gmail.com>
2021-10-26 09:50:10 -0500

    Trying to get delete function to work

--- server/src/data.js
+++ server/src/data.js
@@ -1,27 +1,35 @@
-'use strict';
+"use strict";
 
-const ItemModel = require('./item-model.js');
+const ItemModel = require("./item-model.js");
 
-const Data = { };
+const Data = { ItemModel };
 
-Data.addAnItem = async(req,res,next) => {
+Data.addAnItem = async (req, res, next) => {
   try {
     const data = req.body;
     const item = new ItemModel(data);
     await item.save();
     res.status(200).json(item);
-  } catch(e) { next(e.message); }
-}
+  } catch (e) {
+    next(e.message);
+  }
+};
 
-Data.getAllItems = async(req, res) => {
+Data.getAllItems = async (req, res) => {
   const items = await ItemModel.find({});
   res.status(200).json(items);
-}
+};
 
-Data.getOneItem = async(req, res) => {
+Data.getOneItem = async (req, res) => {
   const id = req.params.id;
-  const items = await ItemModel.find({_id:id});
+  const items = await ItemModel.find({ _id: id });
   res.status(200).json(items[0]);
-}
+};
+
+Data.deleteOneItem = async (req, res) => {
+  const id = req.params.id;
+  const items = await ItemModel.find({ _id: id });
+  res.status(200).json(items[0]);
+};
 
 module.exports = Data;
