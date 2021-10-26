025155ba312e2d9fc638fa4dadcbf4125b520d2c
Defluxit <astroghost1@gmail.com>
2021-10-26 09:46:11 -0500

    Working on adding the delete functionality

--- server/src/server.js
+++ server/src/server.js
@@ -1,24 +1,25 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const cors = require('cors');
-const Data = require('./data');
+const express = require("express");
+const cors = require("cors");
+const Data = require("./data");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/iteems', Data.getAllItems);
-app.get('/items/:id', Data.getOneItem);
-app.post('/items', Data.addAnItem);
+app.get("/iteems", Data.getAllItems);
+app.get("/items/:id", Data.getOneItem);
+app.post("/items", Data.addAnItem);
+app.delete("/items/:id", Data.deleteOneItem);
 
-app.use('*', (req,res) => {
-  res.status(404).send('These are not the droids you are looking for.');
+app.use("*", (req, res) => {
+  res.status(404).send("These are not the droids you are looking for.");
 });
 
-app.use( (error,req,res,next) => {
+app.use((error, req, res, next) => {
   res.status(500).send(`My Bad ... ${error.message}`);
 });
 
